5599ef86edd5985df37355943bef4f28212be765
Wallace676 <wallacestar16@gmail.com>
2025-09-02 14:07:52 +0000

    Parte inicial do super trunfo: O programa permite que o usuário cadastre duas cartas, informando seus atributos, e em seguida imprime os dados preenchidos de cada uma.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,90 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf(" Bem vindo ao Super Trunfo \n"); // Exibe mensagem de boas vindas.
 
-    return 0;
+    char estado[50];
+    char codigo[10];
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int numero_pontos_turisticos;
+
+
+    /* Aqui começa a parte do cadastro de dados 
+    essa seção lê as informações digitadas na carta 1 pelo usuario. */
+    printf("\nPreencha a Carta 1\n");
+
+     printf(" Informe o Estado: ");
+     scanf("%[^\n]", estado); 
+
+     printf(" Informe o Código: ");
+     scanf("%s", codigo);
+
+     printf(" Informe o nome da Cidade: ");
+     scanf(" %[^\n]", cidade);
+
+     printf(" Informe População: ");
+     scanf("%d", &populacao);
+
+     printf(" Informe a Área: ");
+     scanf(" %f", &area);
+     
+     printf(" Informe o PIB: ");
+     scanf("%f", &pib);
+
+     printf(" Informe o Numero de Pontos Turisticos: ");
+     scanf("%d", &numero_pontos_turisticos);
+
+
+     // Essa parte imprime as informações cadastradas na carta 1.
+     printf(" \nCarta 1\n");
+     printf(" Estado: %s\n",estado);
+     printf(" Código: %s\n", codigo);
+     printf(" Nome da Cidade: %s\n", cidade);
+     printf(" População: %d\n", populacao);
+     printf(" Área: %.2f km²\n", area);
+     printf(" PIB: %.3f\n", pib);
+     printf(" Numero de Pontos Turísticos: %d\n", numero_pontos_turisticos);
+ 
+
+    // Essa seção lê as informações digitadas na carta 2 pelo usuário.
+     printf("\nPreencha a Carta 2\n");
+
+     printf(" Informe o Estado: ");
+     scanf(" %[^\n]", &estado);
+
+     printf(" Informe o Código: ");
+     scanf("%s", &codigo);
+
+     printf(" Informe a Cidade: ");
+     scanf(" %[^\n]", &cidade);
+
+     printf(" Informe a População: ");
+     scanf("%d", &populacao);
+
+     printf(" Informe a Área: ");
+     scanf("%f", &area);
+
+     printf(" Informe o PIB: ");
+     scanf("%f", &pib);
+
+     printf(" Informe o Numero de pontos Turísticos: ");
+     scanf("%d", &numero_pontos_turisticos);
+
+     // Essa parte imprime as informações cadastradas na carta 2.
+     printf(" \nCarta 2\n");
+     printf(" Estado: %s\n", estado);
+     printf(" Código: %s\n", codigo);
+     printf(" Nome da Cidade: %s\n", cidade);
+     printf(" População: %d\n", populacao);
+     printf(" Área: %.2f km²\n", area);
+     printf(" PIB: %.3f\n", pib);
+     printf(" Numero de Pontos Turísticos: %d\n", numero_pontos_turisticos);
+
+
+
+     return 0;// Essa parte encerra o programa.
+
 }
